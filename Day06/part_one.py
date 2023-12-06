c8f8dd3c1e7fb50445ed4b40480442b7e50ac16c
Victor Gonzalez Morales <victorgm2709@gmail.com>
2023-12-06 22:31:24 +0100

    Day06

--- Day06/part_one.py
+++ Day06/part_one.py
@@ -1,3 +1,4 @@
+from math import ceil, floor, sqrt
 
 L = open('input.txt').read().split('\n')
 times = list(map(int, L[0].split(':')[1].split()))
@@ -5,10 +6,10 @@ distances = list(map(int, L[1].split(':')[1].split()))
 
 ans = 1
 for (t, d) in zip(times, distances):
-  cnt = 0
-  for i in range(1, t):
-    if (t - i) * i > d:
-      cnt += 1
-  ans *= cnt
+  # (t - i) * i > d -> -i^2 + ti - d > 0 -> i^2 - ti + d > 0
+  s = sqrt(t*t - 4*d)
+  x1 = (t + s) / 2
+  x2 = (t - s) / 2
+  ans *= ceil(x1 - 1) - floor(x2 + 1) + 1
 
 print('Solution:', ans)
--- Day06/part_two.py
+++ Day06/part_two.py
@@ -1,8 +1,14 @@
 
+from math import ceil, floor, sqrt
+
 L = open('input.txt').read().split('\n')
 time = int(''.join(L[0].split(':')[1].split()))
-distance = int(''.join(L[1].split(':')[1].split()))
+record = int(''.join(L[1].split(':')[1].split()))
 
-cnt = sum(1 for i in range(1, time) if (time - i) * i > distance)
+# (t - i) * i > d -> i*i - it + d > 0
+s = sqrt(time*time - 4*record)
+x1 = (time + s) / 2
+x2 = (time - s) / 2
+ans = ceil(x1 - 1) - floor(x2 + 1) + 1
 
-print(f'Solution: {cnt}')
+print(f'Solution: {ans}')
