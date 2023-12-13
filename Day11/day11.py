5629f6b1465d23ffea2829acf45a32006153f180
Victor Gonzalez Morales <victorgm2709@gmail.com>
2023-12-13 20:26:35 +0100

    Day11

--- Day11/day11.py
+++ Day11/day11.py
@@ -1,28 +1,20 @@
 
+size_to_expand = 2
 space_map: list[str]
 
 def read_galaxies(filename: str) -> list[str]:
   return open(filename).read().strip().split('\n')
 
 
-def expand_space() -> None:
+def get_expand_edges() -> (list[int], list[int]):
   expand_i = [i for i, row in enumerate(space_map) 
                 if row.count('#') == 0]
   
   trunc_space = list(zip(*space_map))
   expand_j = [j for j, row in enumerate(trunc_space)
                 if ''.join(row).count('#') == 0]
-  
-  cnt = 0
-  for i in expand_i:
-    space_map.insert(i+cnt, '.' * (len(space_map)-cnt))
-    cnt += 1
-    
-  for i in range(len(space_map)):
-    cnt = 0
-    for j in expand_j:
-      space_map[i] = space_map[i][:j+cnt] + '.' + space_map[i][j+cnt:]
-      cnt += 1  
+
+  return expand_i, expand_j
 
 
 def find_galaxies() -> dict:
@@ -34,12 +26,24 @@ def find_galaxies() -> dict:
   return galaxies
 
 
-def calculate_distance(org: tuple[int, int], dst: tuple[int, int]) -> int:
+def calculate_distance(org: tuple[int, int], dst: tuple[int, int], i_edges: list[int], j_edges: list[int]) -> int:
   (oi, oj), (di, dj) = org, dst
-  return abs(oi-di) + abs(oj-dj)
+  
+  cnt = 0
+  start, end = min(oi, di), max(oi, di)
+  for i in range(start, end):
+    if i in i_edges:
+      cnt += 1
+  
+  start, end = min(oj, dj), max(oj, dj)
+  for j in range(start, end):
+    if j in j_edges:
+      cnt += 1
+  
+  return abs(oi-di) + abs(oj-dj) + cnt * (size_to_expand - 1)
 
 
-def part_one(galaxies: dict) -> int:
+def part_one(galaxies: dict, edges_i: list[int], edges_j: list[int]) -> int:
   galaxy_dist_calculated = set()
   total_distance = 0
   for galaxy_a, coords_a in galaxies.items():
@@ -47,15 +51,18 @@ def part_one(galaxies: dict) -> int:
       if galaxy_a != galaxy_b:
         if (galaxy_b, galaxy_a) not in galaxy_dist_calculated:
           galaxy_dist_calculated.add((galaxy_a, galaxy_b))
-          distance = calculate_distance(coords_a, coords_b)
+          distance = calculate_distance(coords_a, coords_b, edges_i, edges_j)
           total_distance += distance
   return total_distance
 
 
 if __name__ == '__main__':
   space_map = read_galaxies('input.txt')
-  expand_space()
+  exp_i, exp_j = get_expand_edges()
   galaxies_coords = find_galaxies()
-  ans = part_one(galaxies_coords)
+  ans = part_one(galaxies_coords, exp_i, exp_j)
   print(f'Part one: {ans}')
+  size_to_expand = 1000000
+  ans = part_one(galaxies_coords, exp_i, exp_j)
+  print(f'Part two: {ans}')
   
