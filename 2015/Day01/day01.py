aa3646fbece8986665703f2792215180c8279ed7
Victor Gonzalez Morales <victorgm2709@gmail.com>
2024-12-07 01:52:26 +0100

    Day01

--- 2015/Day01/day01.py
+++ 2015/Day01/day01.py
@@ -10,11 +10,19 @@ def pr(s: str, ans: int):
     print(s, ans)
     cp.copy(ans)
 
-in1 = open(sys.argv[1]).read().strip().split()
+in1 = open(sys.argv[1]).read().strip()
 
 def mapper(p: str) -> int:
     return 1 if p == "(" else -1
 
-p1 = sum(x for row in in1 for x in map(mapper, row))
+p1 = sum(x for x in map(mapper, in1))
+
+lvl = 0
+for i, p in enumerate(in1, start=1):
+    lvl += mapper(p)
+    if lvl == -1:
+        p2 = i
+        break
 
 pr("Part One:", p1)
+pr("Part Two:", p2)
