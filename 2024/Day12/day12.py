1cfd2b7ecaa213fcbd76c70c041861f8d1345a82
Victor Gonzalez Morales <victorgm2709@gmail.com>
2024-12-13 22:33:30 +0100

    Day12

--- 2024/Day12/day12.py
+++ 2024/Day12/day12.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 import sys
 import pyperclip as cp
 
@@ -17,74 +16,81 @@ Coord = tuple[int, int]
 
 class Region:
   
-  
   def __init__(self, letter="#"):
     self.area: int = 0
-    self.side: int = 0
-    self.letter: str = "#"
+    self.letter: str = letter
+    self.total_sides: int = 0
+        
     self.perimeter: int = 0
-    self.diagonal: set[Coord] = set()
-    self.contour: set[Coord] = set()
     self.structure: set[Coord] = set()
-    self.contour_freq: defaultdict[int] = defaultdict(int)
-
-    self.tl, br = (0, 0), (0, 0)
-    self.extremes_calculated = False
-
-    self.matrix = None
-    self.matrix_drew = False
-    self.letter = letter
     
-  def calculate_extremes(self) -> tuple[int, int, int, int]:
-    if self.extremes_calculated:
-      return *self.tl, *self.br
-    self.extremes_calculated = True
+    self.top: set[Coord] = set()
+    self.left: set[Coord] = set()
+    self.right: set[Coord] = set()
+    self.bottom: set[Coord] = set()
+    
+    self.matrix = None
+    
+  def _calculate_extremes(self) -> tuple[int, int, int, int]:
     
     mmi, mmj, mxi, mxj = float('inf'), float('inf'), -float('inf'), -float('inf')
     for i, j in self.structure:
       mmi, mmj = min(mmi, i), min(mmj, j)
       mxi, mxj = max(mxi, i), max(mxj, j)
     
-    self.tl = (mmi, mmj)
-    self.br = (mxi, mxj)
     return mmi, mmj, mxi, mxj
   
-  def draw(self, n=None, m=None):
-    if n is not None and m is not None:
-      matrix = [[0 for _ in range(m)] for _ in range(n)]
-      for i, j in self.structure:
-        matrix[i][j] = 1
+  def draw(self):
 
-    elif not self.matrix_drew:
-      self.matrix_drew = True
-      mmi, mmj, mxi, mxj = self.calculate_extremes()
-      idiff, jdiff = mmi, mmj
-      
-      matrix = [[0 for _ in range(mxj+1)] for _ in range(mxi+1)]
-      for i, j in self.structure:
-        i, j = i-idiff, j-jdiff
-        matrix[i][j] = 1
-        
+    mmi, mmj, mxi, mxj = self._calculate_extremes()
+    idiff, jdiff = mmi, mmj
+    
+    matrix = [[' ' for _ in range(mxj-mmj+1)] for _ in range(mxi-mmi+1)]
+    for i, j in self.structure:
+      i, j = i-idiff, j-jdiff
+      matrix[i][j] = self.letter
+    
+    print("\n+", "-" * (mxj-mmj+1), "+")
     for row in matrix:
+      line = "| "
       for val in row:
-        print(' ' if not val else self.letter, end='')
-      print()
+        line += val
+      print(line, "|")
+    print("+", "-" * (mxj-mmj+1), "+", "\n")
   
   def calculate_sides(self) -> int:
-    print("Diagonal:", self.diagonal)
-    print("Contorno:", self.contour)
-    print("Freq:", self.contour_freq)
-    print("Corners Filtrado:", [(i, j) for (i, j), f in self.contour_freq.items() if f == 1 and (i, j) in self.diagonal])
-    print("\n")
-    # contour = self.contour.copy()
-    # side = 1
+    print("Area:", self.area)
+    print("Structure:", self.structure)
+  
+    sides: list[set[Coord]] = [] 
+    #                      top      bot    left     right
+    for chk_i, chk_j in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+      side_dir: set[Coord] = set()
+      for i, j in self.structure:
+        if (i+chk_i, j+chk_j) not in self.structure:
+          side_dir.add((i, j))
+      sides.append(side_dir.copy())
+      
+    print(sides)
     
-    # i, j = contour.pop()
-    # while contour:
-    #   for ni, nj in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-    #     if (ni, nj) in contour:
-          
-    #       i, j = ni, nj
+    for side in sides:
+      while side:
+        i, j = side.pop()
+        
+        neighbours: set[Coord] = set([(i, j)])
+        while neighbours:
+          i, j = neighbours.pop()
+          if (i, j) in side:
+            side.remove((i, j))
+          for ni, nj in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+            if (ni, nj) in side:
+              neighbours.add((ni, nj))
+        
+        self.total_sides += 1
+    
+    print("Total sides:", self.total_sides, "\n")
+    return self.total_sides
+    
           
  
 
@@ -125,18 +131,9 @@ def part_two(garden: list[str]) -> int:
     visited.add((i, j))
     region.area += 1
     
-    # check diagonally
-    for ni, nj in [(i-1, j-1), (i-1, j+1), (i+1, j-1), (i+1, j+1)]:
-      if (ni < 0 or ni == n or nj < 0 or nj == m) or garden[ni][nj] != garden[i][j]:
-        region.diagonal.add((ni, nj))
-        region.contour.add((ni, nj))
-        region.contour_freq[(ni, nj)] += 1
-    
     # check neighbour
     for ni, nj in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
       if (ni < 0 or ni == n or nj < 0 or nj == m) or garden[ni][nj] != garden[i][j]:
-        region.contour.add((ni, nj))
-        region.contour_freq[(ni, nj)] += 1
         region.perimeter += 1
         continue
       
@@ -149,9 +146,9 @@ def part_two(garden: list[str]) -> int:
       if (i, j) not in visited:
         region = Region(letter)
         dfs(i, j, region)
-        region.draw(n, m)
+        region.draw()
         region.calculate_sides()
-        # total_price += region.area * region.side
+        total_price += region.area * region.total_sides
   
   return total_price
 
@@ -161,7 +158,7 @@ if __name__ == '__main__':
   if len(sys.argv) < 2:
     print("Usage: python3 dayXX.py input.in")
     exit()
-  
+  # filename = "./test2.in"
   for filename in sys.argv[1:]:
     print(f"Filename: [ {filename} ]")
     input = read_file(filename)
