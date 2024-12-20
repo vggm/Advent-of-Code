45a665a465b35026999a29ebe20ce07f23d77c8b
Victor Gonzalez Morales <victorgm2709@gmail.com>
2024-12-20 01:41:28 +0100

    Day19

--- 2024/Day19/day19.py
+++ 2024/Day19/day19.py
@@ -86,12 +86,11 @@ def part_two(file_input: list[str]) -> int:
 
 if __name__ == '__main__':
   
-  # if len(sys.argv) < 2:
-  #   print("Usage: python3 dayXX.py input.in")
-  #   exit()
+  if len(sys.argv) < 2:
+    print("Usage: python3 dayXX.py input.in")
+    exit()
   
-  # file_input = read_file(sys.argv[1])
-  file_input = read_file("./input.in")
+  file_input = read_file(sys.argv[1])
   pr("Part One:", part_one(file_input))
   pr("Part Two:", part_two(file_input))
   
