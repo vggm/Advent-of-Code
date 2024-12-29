798ba1e9a873f66be39dfa09c4a5d54ff1b5f0e8
Victor Gonzalez Morales <victorgm2709@gmail.com>
2024-12-29 20:04:23 +0100

    Day22

--- 2024/Day22/day22.py
+++ 2024/Day22/day22.py
@@ -10,12 +10,41 @@ def pr(s: str, ans: int):
   print(s, ans)
   cp.copy(ans)
  
+ 
+PRUNE_NUM = 16777216
+ 
 
-def part_one(file_input: list[str]) -> int:
-  return -1
+def part_one(secrets: list[str]) -> int:
+  
+  total_sum = 0
+  for secret in map(int, secrets):
+    org_secret = secret
+    for _ in range(2_000):
+      old_secret = secret
+      
+      secret *= 64
+      secret ^= old_secret
+      secret %= PRUNE_NUM
+      
+      old_secret = secret
+      
+      secret //= 32
+      secret ^= old_secret
+      secret %= PRUNE_NUM
+      
+      old_secret = secret
+      
+      secret *= 2048
+      secret ^= old_secret
+      secret %= PRUNE_NUM
+      
+    # print(org_secret, ":", secret)
+    total_sum += secret
+  
+  return total_sum
 
 
-def part_two(file_input: list[str]) -> int:
+def part_two(secrets: list[str]) -> int:
   return -1
 
 
