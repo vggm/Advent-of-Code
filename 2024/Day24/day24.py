f5323776662b33d945c877b4dc80c397d0b4ecec
Victor Gonzalez Morales <victorgm2709@gmail.com>
2024-12-30 19:20:59 +0100

    Day24

--- 2024/Day24/day24.py
+++ 2024/Day24/day24.py
@@ -1,6 +1,7 @@
 from collections import deque
-import sys
+from functools import cache
 import pyperclip as cp
+import sys
 
 
 def read_file(filename: str) -> list[str]:
@@ -35,33 +36,112 @@ def do_operation(a: bool, b: bool, op: str) -> bool:
   return res
 
 
+def dict_to_bin(nums_dict: dict[str, bool], num: str) -> str:
+  num_values = {k: v for k, v in nums_dict.items() if k[0] == num}
+  
+  res = []
+  for k in sorted(num_values, reverse=True):
+    res.append("1" if num_values[k] else "0")
+  
+  return "".join(res)
+
+
+def get_index(l: list[str], t: str) -> int:
+  for i, val in enumerate(l):
+    if val == t:
+      return i
+  
+  return -1
+
+
 def part_one(file_input: str) -> int:
   values, operations = get_params(file_input)
   
-  max_z = 0
   while operations:
     operation, result = operations.popleft()
     a, op, b = operation.split()
+    
     if a not in values or b not in values:
       operations.append((operation, result))
       continue
     
-    if result[0] == "z":
-      max_z = max(max_z, int(result[1:]))
-    
     values[result] = do_operation(values[a], values[b], op)
-    
-  res = [""] * (max_z + 1)
-  for k, v in values.items():
-    if k[0] == "z":
-      index = max_z - int(k[1:])
-      res[index] = "1" if v else "0"
 
-  return int("".join(res), 2)
+  return int(dict_to_bin(values, "z"), 2)
 
 
 def part_two(file_input: list[str]) -> int:
-  return -1
+  values, operations = get_params(file_input)
+  
+  temp_vl = values.copy()
+  temp_op = operations.copy()
+  
+  while operations:
+    operation, result = operations.popleft()
+    a, op, b = operation.split()
+    
+    if a not in values or b not in values:
+      operations.append((operation, result))
+      continue
+    
+    values[result] = do_operation(values[a], values[b], op)
+    
+  x, y = dict_to_bin(values, "x"), dict_to_bin(values, "y")
+  target = str(bin(int(x,2) + int(y,2)))[2:]
+  
+  seen = set()
+  variables = [var for _, var in temp_op]
+  taken = [False] * len(variables)
+  def bt(e: int, n: int, parcial: list[str]) -> tuple:
+    if n == 4:
+      s_par = set(parcial)
+      
+      values = temp_vl.copy()
+      operations = temp_op.copy()
+      
+      while operations:
+        operation, result = operations.popleft()
+        
+        if result in s_par:
+          index = get_index(parcial, result)
+          n_result = parcial[index-1] if index % 2 == 1 else parcial[index+1]
+        
+        a, op, b = operation.split()
+        
+        if a not in values or b not in values:
+          operations.append((operation, result))
+          continue
+      
+        if result not in s_par:
+          values[result] = do_operation(values[a], values[b], op)
+        else:
+          values[n_result] = do_operation(values[a], values[b], op)
+      
+      z = dict_to_bin(values, "z")
+      return parcial if z == target else []
+    
+    for i, a in enumerate(variables[e:], start=e):
+      if taken[i]:
+        continue
+      taken[i] = True
+      for j, b in enumerate(variables):
+        if taken[j] or (a, b) in seen or (b, a) in seen:
+          continue
+        taken[j] = True
+        
+        # seen.add((a, b, n))
+        # seen.add((b, a, n))
+        
+        if sol := bt(i+1, n+1, parcial + [a] + [b]):
+          return sol
+        
+        taken[j] = False
+      taken[i] = False
+      
+    return []
+          
+  
+  return ",".join(sorted(bt(0, 0, [])))
 
 
 if __name__ == '__main__':
@@ -71,6 +151,7 @@ if __name__ == '__main__':
     exit()
   
   file_input = read_file(sys.argv[1])
-  pr("Part One:", part_one(file_input))
-  # pr("Part Two:", part_two(file_input))
+  # pr("Part One:", part_one(file_input))
+  pr("Part Two:", part_two(file_input))
+  # pr("Part Two:", part_two(read_file("./test3.in")))
   
