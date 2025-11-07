ee8280cc03c7f4c02d0a5b00b9306e7b69771619
Victor Gonzalez Morales <victorgm2709@gmail.com>
2025-11-07 02:13:43 +0100

    2018 Day07 Part1

--- 2018/Day07/day07.py
+++ 2018/Day07/day07.py
@@ -2,49 +2,54 @@ import re
 from collections import defaultdict 
 from heapq import heappush, heappop
 
-with open("./test.txt", "r") as fr:
+with open("./input.txt", "r") as fr:
     lines = fr.readlines()
 
 nodes = set()
 graph = defaultdict(set)
+pre_req = defaultdict(int)
 for line in lines:
     org, dst = re.search(r'Step ([A-Z]).*step ([A-Z])', line).groups()
+    
+    pre_req[dst] += 1
     graph[org] |= {dst}
     nodes |= {org, dst}
 
 
 # =========== Part One =========== #
 
-print("Graph:")
-for key, values in graph.items():
-    print(f"{key}: {values}")
-print()
+# print("Graph:")
+# for key, values in graph.items():
+#     print(f"{key}: {values}")
+# print()
+
+# print("Pre Req:")
+# for key, value in pre_req.items():
+#     print(f"{key}: {value}")
+# print()
 
 start: set[str] = nodes.copy()
-for org, dst in graph.items():
-    start ^= dst
+for org, dst_list in graph.items():
+    for dst in dst_list:
+        if dst in start:
+            start.remove(dst) # dst node never must be a start node
 
-print(f"{start=}")
+# print(f"{start=}\n")
 
-start: str = start.pop()
-seen: set[str] = set()
+stack = []
+for node in start:
+    #               (alphabet_order, curr_node)
+    heappush(stack, (ord(node), node))
 
-# (alphabet_order, curr_node, path)
-stack = [(ord(start), start, "")]
+path = ""
 while stack:
-    _, node, path = heappop(stack)
-    print(node)
-    
-    if node in seen:
-        continue
-    seen.add(node)
+    _, node = heappop(stack)
     
     path += node
     
-    if len(path) == len(nodes):
-        break
-    
     for nxt in graph[node]:
-        heappush(stack, (ord(nxt), nxt, path))
+        pre_req[nxt] -= 1
+        if pre_req[nxt] == 0: # does not have pre req
+            heappush(stack, (ord(nxt), nxt))
     
 print(f"Part One: {path}")
