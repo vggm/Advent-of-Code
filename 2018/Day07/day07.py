ca00680c7e434db27996ad69c4a48638b38a5e51
Victor Gonzalez Morales <victorgm2709@gmail.com>
2025-11-07 01:41:34 +0100

    2018 Day07

--- 2018/Day07/day07.py
+++ 2018/Day07/day07.py
@@ -1,5 +1,6 @@
 import re
 from collections import defaultdict 
+from heapq import heappush, heappop
 
 with open("./test.txt", "r") as fr:
     lines = fr.readlines()
@@ -11,13 +12,39 @@ for line in lines:
     graph[org] |= {dst}
     nodes |= {org, dst}
 
+
+# =========== Part One =========== #
+
 print("Graph:")
 for key, values in graph.items():
     print(f"{key}: {values}")
 print()
 
-start = nodes.copy()
+start: set[str] = nodes.copy()
 for org, dst in graph.items():
     start ^= dst
 
 print(f"{start=}")
+
+start: str = start.pop()
+seen: set[str] = set()
+
+# (alphabet_order, curr_node, path)
+stack = [(ord(start), start, "")]
+while stack:
+    _, node, path = heappop(stack)
+    print(node)
+    
+    if node in seen:
+        continue
+    seen.add(node)
+    
+    path += node
+    
+    if len(path) == len(nodes):
+        break
+    
+    for nxt in graph[node]:
+        heappush(stack, (ord(nxt), nxt, path))
+    
+print(f"Part One: {path}")
