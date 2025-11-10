5580eeba409e32f8478cd6d2cfe5014ec76b7e52
Victor Gonzalez Morales <victorgm2709@gmail.com>
2025-11-10 16:15:30 +0100

    2018 Day10

--- 2018/Day10/day10.py
+++ 2018/Day10/day10.py
@@ -4,25 +4,25 @@ with open("./test.txt", "r") as fr:
     lines = fr.readlines()
 
 
+POSITION, VELOCITY = 0, 1
+
 points = []
-velocities = []
 for line in lines:
     position, velocity = re.findall(r"<\s*([-]?[0-9]+),\s*([-]?[0-9]+)>", line)
     position, velocity = list(map(int, position)), list(map(int, velocity))
     
     # print(position, velocity)
     
-    points.append(position)
-    velocities.append(velocity)
+    points.append([position, velocity])
 
 top, right = 0, 0 # limits
-for j, i in points:
+for (j, i), (_, _) in points:
     top = max(top, i)
     right = max(right, j)
 
 m = [["." for _ in range(right + 1)] for _ in range(top + 1)]
 
-for idx, (j, i) in enumerate(points):
+for (j, i), (_, _) in points:
     m[i][j] = "#"
     
 for row in m:
