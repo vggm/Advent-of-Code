85f1298ae695e851e621df51695595af8081fc10
Victor Gonzalez Morales <victorgm2709@gmail.com>
2025-11-12 14:00:25 +0100

    2018 Day10

--- 2018/Day10/day10.py
+++ 2018/Day10/day10.py
@@ -1,4 +1,8 @@
 import re
+from collections import defaultdict
+import numpy as np
+import cv2 as cv
+
 
 with open("./input.txt", "r") as fr:
     lines = fr.readlines()
@@ -31,20 +35,11 @@ for position in points:
     position[1] -= left
 
 print(f"{top=}, {bottom=}, {left=}, {right=}\n{real_top=}, {real_right=}\n")
-m = [[0 for _ in range(real_right)] for _ in range(real_top)]
 
+
+graph = defaultdict(int)
 for i, j in points:
-    m[i][j] = 1
-
-def show_matrix(idx="#"):
-    print("=" * 20, str(idx), "=" * 20)
-    for row in m:
-        for val in row:
-            print(f"{"#" if val > 0 else "."}", end=" ")
-        print()
-    print("=" * 43)
-    
-# show_matrix(0)
+    graph[(i, j)] += 1
 
 more_than_one = False
 
@@ -52,50 +47,60 @@ def make_move():
     more_than_one = False
     for idx, (di, dj) in enumerate(velocities):
         i, j = points[idx]
-        m[i][j] -= 1 # remove old position
+        graph[(i, j)] -= 1 # remove old position
+        
+        if graph[(i, j)] == 0:
+            del graph[(i, j)]
         
         ni, nj = (i+di) % real_top, (j+dj) % real_right
-        m[ni][nj] += 1 # check the new position
+        graph[(ni, nj)] += 1 # check the new position
         
-        if not more_than_one and m[ni][nj] > 1:
+        if not more_than_one and graph[(ni, nj)] > 1:
             more_than_one = True
         
         points[idx] = [ni, nj] # update point
-    
-# make_move()
-# show_matrix()
 
-
-def is_two_island() -> bool:
-    cnt = 0
-    seen = set()
-    
-    def explore(i, j):
-        nonlocal seen
-        seen.add((i, j))
-        
-        for ni, nj in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-            if 0 <= ni < real_top and 0 <= nj < real_right and m[ni][nj] == 1 and (ni, nj) not in seen:
-                explore(ni, nj)
-    
-    for i in range(real_top):
-        for j in range(real_right):
-            if m[i][j] == 1 and (i, j) not in seen:
-                explore(i, j)
-                cnt += 1
-                if cnt > 2:
-                    return False
-    return True
-
-from tqdm import tqdm
-
-for i in tqdm(range(1, 10_000)):
-    
-    if i % 1000 == 0:
-        print(i)
-    
+for i in range(3):
     make_move()
     
-    if not more_than_one and is_two_island():
-        show_matrix(i)
+
+    
+# vt, vb, vl, vr = 100_000, 0, 100_000, 0
+# for i, j in points:
+#     vt = min(vt, i)
+#     vb = max(vb, i)
+#     vl = min(vl, j)
+#     vr = max(vr, j)
+
+
+# height, width = vb-vt, vr-vl
+
+# print(f"{vt=}, {vb=}, {vl=}, {vr=}")
+# print(f"Image shape: ({height:_}x{width:_})")
+# print(f"Image shape: ({real_top:_}x{real_right:_})")
+# mx = np.zeros((height, width), dtype=np.uint)
+# mx = np.zeros((real_top, real_right), dtype=np.uint)
+
+# for i, j in points:
+#     mx[i, j] = 255
+
+# mh, mw = real_top // 2, real_right // 2
+# top_left = mx[:mh, :mw]
+# top_right = mx[:mh, mw:]
+# bottom_left = mx[mh:, :mw]
+# bottom_right = mx[mh:, mw:]
+
+# from matplotlib import pyplot as plt
+# from collections import Counter
+
+# cnt = {0: 0, 255: 0}
+# for row in top_left:
+#     for val in row:
+#         cnt[val] += 1
         
+# print(cnt)
+
+# # cv.imwrite("top_left.tiff", top_left)
+# # cv.imwrite("top_right.tiff", top_right)
+# # cv.imwrite("bottom_left.tiff", bottom_left)
+# # cv.imwrite("bottom_right.tiff", bottom_right)
