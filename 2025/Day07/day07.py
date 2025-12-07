e02d45c2417ffc6ba356aa722a92ae6f45f54ddd
Victor Gonzalez Morales <victorgm2709@gmail.com>
2025-12-07 17:31:12 +0100

    2025 Day07

---
+++ 2025/Day07/day07.py
@@ -0,0 +1,185 @@
+
+from collections import deque
+
+
+with open("./input.txt") as fr:
+  lines = fr.read().strip().split("\n")
+
+SPLIT_SYMBOL = "^"
+START_SYMBOL = "S"
+ROWS, COLS = len(lines), len(lines[0])
+
+splitters = []
+for i, row in enumerate(lines):
+  for j, val in enumerate(row):
+    if val == SPLIT_SYMBOL:
+      splitters.append((i, j))
+      
+      
+start_col = lines[0].find(START_SYMBOL)
+      
+  
+# =========== Part One =========== #
+
+#########################################
+## First Approach: Following the beams ##
+#########################################
+
+# find the first split
+active_splitter = set()
+for i in range(1, ROWS):
+  if lines[i][start_col] == SPLIT_SYMBOL:
+    active_splitter.add((i, start_col))
+    break
+
+
+seen = set()
+def search_split(i: int, j: int) -> tuple[int, int]:
+  if (i, j) in seen:
+    return -1, -1
+  
+  seen.add((i, j))
+  for ni in range(i+1, ROWS):
+    if lines[ni][j] == SPLIT_SYMBOL:
+      return ni, j
+  
+  return -1, -1
+
+
+def check_both_sides(i: int, j: int) -> list[tuple[int, int]]:
+  # left side
+  if 0 <= j - 1: 
+    li, lj = search_split(i, j-1)
+  
+  # right side
+  if j + 1 < COLS: 
+    ri, rj = search_split(i, j+1)
+  
+  return [(li, lj), (ri, rj)]
+
+
+for i, j in splitters:
+  if (i, j) not in active_splitter:
+    continue
+  
+  left_beam, right_beam = check_both_sides(i, j)
+  
+  if left_beam != (-1, -1):
+    active_splitter.add(left_beam)
+  
+  if right_beam != (-1, -1):
+    active_splitter.add(right_beam)
+  
+    
+print(f"Part One [First Approach]: {len(active_splitter)}")
+
+
+###################################
+## Second Approach: Using BTrees ##
+###################################
+
+class TNode:
+  def __init__(self, left=None, right=None, coords=None):
+    self.left = left
+    self.right = right
+    self.coords = coords
+    
+
+seen: dict[tuple[int, int], TNode] = {}
+nodes: dict[tuple[int, int], TNode] = {}
+def search_split(i: int, j: int) -> TNode | None:
+  if (i, j) in seen:
+    return seen[(i, j)]
+  
+  for ni in range(i+1, ROWS):
+    if lines[ni][j] == SPLIT_SYMBOL:
+      nodes[(ni, j)] = TNode(coords=(ni, j))
+      seen[(i, j)] = nodes[(ni, j)]
+      return nodes[(ni, j)]
+  
+  return None
+
+
+def check_both_sides(i: int, j: int) -> list[TNode | None, TNode | None]:
+  # left side
+  if 0 <= j - 1: 
+    left_node = search_split(i, j-1)
+  
+  # right side
+  if j + 1 < COLS: 
+    right_node = search_split(i, j+1)
+  
+  return [left_node, right_node]
+
+    
+# find the first split or root node
+for i in range(1, ROWS):
+  if lines[i][start_col] == SPLIT_SYMBOL:
+    root = TNode(coords=(i, start_col))
+    break 
+
+# build tree
+nodes[root.coords] = root
+
+visited = set()
+nodes_to_explore: deque[TNode] = deque([root])
+while nodes_to_explore:
+  curr_node = nodes_to_explore.popleft()
+  
+  left_beam, right_beam = check_both_sides(*curr_node.coords)
+  
+  if left_beam is not None:
+    curr_node.left = left_beam
+    if left_beam.coords not in visited:
+      nodes_to_explore.append(left_beam)
+      visited.add(left_beam.coords)
+  
+  if right_beam is not None:
+    curr_node.right = right_beam
+    if right_beam.coords not in visited:
+      nodes_to_explore.append(right_beam)
+      visited.add(right_beam.coords)
+
+seen = set()
+def count_nodes(node: TNode) -> int:
+  if node is None:
+    return 0
+  
+  if node.coords in seen:
+    return 0
+  
+  seen.add(node.coords)
+  return 1 + count_nodes(node.left) + count_nodes(node.right)
+
+n_nodes = 0
+visited = set()
+stack = deque([root])
+while stack:
+  curr = stack.popleft()
+  n_nodes += 1
+  
+  if curr.left is not None and curr.left.coords not in visited:
+    stack.append(curr.left)
+    visited.add(curr.left.coords)
+    
+  if curr.right is not None and curr.right.coords not in visited:
+    stack.append(curr.right)
+    visited.add(curr.right.coords)
+
+# the answer is len(nodes)... but a bfs function is cooler
+print(f"Part One [BTree Approach]: {n_nodes}")
+
+
+# =========== Part Two =========== #
+
+def count_none_childs(node: TNode, memo={}) -> int:  
+  if node is None:
+    return 1
+  
+  if node.coords in memo:
+    return memo[node.coords]
+  
+  memo[node.coords] = count_none_childs(node.left) + count_none_childs(node.right)
+  return memo[node.coords]
+  
+print(f"Part Two [BTree Approach]: {count_none_childs(root)}")
