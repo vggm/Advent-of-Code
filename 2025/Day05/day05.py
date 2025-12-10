d1ef9166062182f6dcd4133b0672066c61f59ad2
Victor Gonzalez Morales <victorgm2709@gmail.com>
2025-12-10 17:57:40 +0100

    2025 Day05

--- 2025/Day05/day05.py
+++ 2025/Day05/day05.py
@@ -20,6 +20,18 @@ print(f"Part One: {cnt}")
 
 # =========== Part Two =========== #
 
-cnt = 0
+fresh_list.sort()
+fresh_list_opt = []
+
+L, R = fresh_list[0]
+for l1, l2 in fresh_list[1:]:
+  if R < l1: # cannot join, its a new range
+    fresh_list_opt.append((L, R)) # save last range
+    L, R = l1, l2 # start the new range
+    
+  else:
+    R = max(R, l2) # join ranges if l2 > R2, else L <= l1, l2 <= R
+
+fresh_list_opt.append((L, R))
       
-print(f"Part Two: {cnt}")
+print(f"Part Two: {sum(R-L+1 for L, R in fresh_list_opt)}")
