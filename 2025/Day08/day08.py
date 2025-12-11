bf307053b85741ce43b50ddf976575de17b0a0a9
Victor Gonzalez Morales <victorgm2709@gmail.com>
2025-12-11 14:34:19 +0100

    2025 Day08

--- 2025/Day08/day08.py
+++ 2025/Day08/day08.py
@@ -1,10 +1,8 @@
-from collections import defaultdict
 from functools import reduce
-from tqdm import tqdm
 from math import sqrt
 
 
-OPT = 0 # 0 for test, 1 for input
+OPT = 1 # 0 for test, 1 for input
 INPUT_FILE, N_CONN = (("./test.txt", 10), ("./input.txt", 1000))[OPT] 
 
 
@@ -16,100 +14,64 @@ junction_boxes = list(map(lambda x: tuple(map(int, x.split(","))), junction_boxe
   
 # =========== Part One =========== #
 
-def calculate_euclidian_distance(box1, box2) -> int:
+def euclidian_distance(box1, box2) -> int:
   x1, y1, z1 = box1
   x2, y2, z2 = box2
   return sqrt(pow(x1-x2, 2) + pow(y1-y2, 2) + pow(z1-z2, 2))
 
 
-circuits    = {box_id: {(x, y, z)} for box_id, (x, y, z) in enumerate(junction_boxes)}
-box_circuit = {(x, y, z): box_id   for box_id, (x, y, z) in enumerate(junction_boxes)}
+circuits    = {circ_id: {(x, y, z)} for circ_id, (x, y, z) in enumerate(junction_boxes)}
+circuit_id  = {(x, y, z): circ_id   for circ_id, (x, y, z) in enumerate(junction_boxes)}
 
 
 # calculate all distances just one time
 distance: dict = {
-  (box1, box2): calculate_euclidian_distance(box1, box2)
+  (box1, box2): euclidian_distance(box1, box2)
     for i, box1 in enumerate(junction_boxes) 
       for box2 in junction_boxes[i+1:]}
-  
-# for i, box1 in enumerate(junction_boxes): 
-#   for box2 in junction_boxes[i+1:]:
-#     distance[(box2, box1)] = distance[(box1, box2)]
 
-circuits_id_idx = 1
 sorted_distances = sorted([(dist, pair) for pair, dist in distance.items()])
     
-with open("distances.txt", "w") as fw:
-  for dist, (box1, box2) in sorted_distances:
-    fw.write(f"{box1}, {box2}: {dist}\n")
     
 for dist, (box1, box2) in sorted_distances[:N_CONN]:
-  circuit_id_box1, circuit_id_box2 = box_circuit[box1], box_circuit[box2]
+  circuit_id_box1, circuit_id_box2 = circuit_id[box1], circuit_id[box2]
+   
+  if circuit_id_box1 != circuit_id_box2:
+    circuits[circuit_id_box1] |= circuits[circuit_id_box2] # join circuits
+    
+    for box in circuits[circuit_id_box2]:
+      circuit_id[box] = circuit_id_box1 # update value
+    
+    del circuits[circuit_id_box2]
+
+
+circuits_len = list(map(lambda x: len(x), circuits.values()))
+circuits_len.sort(reverse=True) # desc order
+
+print(f"Part One: {reduce(lambda x, y: x*y, circuits_len[:3])}")
+
+
+# =========== Part Two =========== #
+
+circuits    = {box_id: {(x, y, z)} for box_id, (x, y, z) in enumerate(junction_boxes)}
+circuit_id  = {(x, y, z): box_id   for box_id, (x, y, z) in enumerate(junction_boxes)}
+
+
+for dist, (box1, box2) in sorted_distances:
+  
+  circuit_id_box1, circuit_id_box2 = circuit_id[box1], circuit_id[box2]
    
   if circuit_id_box1 != circuit_id_box2:
     circuits[circuit_id_box1] |= circuits[circuit_id_box2]
     
     for box in circuits[circuit_id_box2]:
-      box_circuit[box] = circuit_id_box1
+      circuit_id[box] = circuit_id_box1
     
     del circuits[circuit_id_box2]
-
-# for key, val in circuits.items():
-#   print(f"{key}: {val} {len(val)}")
-#   print()
-
-print(f"Part One: {reduce(lambda x, y: x*y, sorted(list(map(lambda x: len(x), circuits.values())), reverse=True)[:3])}")
-
-
-# =========== Part Two =========== #
-
-circuits = {0: {(x, y, z) for x, y, z in junction_boxes}}
-box_circuit = {(x, y, z): 0 for x, y, z in junction_boxes}
-
-i = 0
-while len(circuits) > 1:
-  dist, (box1, box2) = sorted_distances[i]
   
-  if box1 in [(216,146,977), (117,168,530)] and box2 in [(216,146,977), (117,168,530)]:
-    print(circuits)
+    if len(circuits) == 1:
+      break
   
-  circuit_id_box1, circuit_id_box2 = box_circuit[box1], box_circuit[box2]
   
-  if circuit_id_box1 == circuit_id_box2 and circuit_id_box1 != 0:
-    i += 1
-    continue
-  
-  if circuit_id_box1 == 0 and circuit_id_box2 == 0:
-    circuits[0].remove(box1)
-    circuits[0].remove(box2)
-    
-    circuits[circuits_id_idx] = {box1, box2}
-    box_circuit[box1] = circuits_id_idx
-    box_circuit[box2] = circuits_id_idx
-    
-    circuits_id_idx += 1
-  
-  elif circuit_id_box1 != circuit_id_box2 and 0 in [circuit_id_box1, circuit_id_box2]:
-    circuit_id_org, circuit_id_dst = (circuit_id_box1, circuit_id_box2) if circuit_id_box1 == 0 else (circuit_id_box2, circuit_id_box1)
-    box_org = box1 if circuit_id_box1 == 0 else box2
-    
-    circuits[circuit_id_org].remove(box_org)
-    circuits[circuit_id_dst].add(box_org)
-    
-    box_circuit[box_org] = circuit_id_dst
-  
-  else:
-    circuits[circuit_id_box1] |= circuits[circuit_id_box2]
-    
-    for box in circuits[circuit_id_box2]:
-      box_circuit[box] = circuit_id_box1
-    
-    del circuits[circuit_id_box2]
-  if box1 in [(216,146,977), (117,168,530)] and box2 in [(216,146,977), (117,168,530)]:
-    print(circuits)
-  i += 1
-  
-
-print(circuits)
-print(box1, box2)
 print(f"Part Two: {box1[0] * box2[0]}")
+print("Last boxes:", box1, box2)
