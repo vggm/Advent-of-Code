cda53568f9583e7a4af5f4af7589eec1fe3024e7
Victor Gonzalez Morales <victorgm2709@gmail.com>
2025-12-31 20:54:05 +0100

    2025 Day12

--- 2025/Day12/day12.py
+++ 2025/Day12/day12.py
@@ -1,16 +1,29 @@
 
-with open("./test.txt") as fr:
+with open("./input.txt") as fr:
   sections = fr.read().strip().split("\n\n\n")
 
 first_section = sections[0].split("\n\n")
 second_section = sections[1].split("\n")
 
 present_shapes = [shape.split("\n")[1:] for shape in first_section]
+present_area = [sum(row.count("#") for row in shape) for shape in present_shapes]
+
 regions = [tuple(map(int, region.split("x"))) for region in list(map(lambda x: x.split(": ")[0], second_section))]
 quantities = [tuple(map(int, quantity.split(" "))) for quantity in list(map(lambda x: x.split(": ")[1], second_section))]
   
+  
 # =========== Part One =========== # 
   
+valid_regions = 0
+for (height, width), list_of_quantity in zip(regions, quantities):
+  area = height * width
+  sum_area = sum(present_area[idx] * qnt for idx, qnt in enumerate(list_of_quantity))
+  
+  if sum_area <= area:
+    valid_regions += 1
+
+print(f"Part One: {valid_regions}")
+
 
 # =========== Part Two =========== #
 
