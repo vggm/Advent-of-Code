4728b6e8ba2167680bc6e6ca21fb0ef337f7613d
Victor Gonzalez Morales <victorgm2709@gmail.com>
2026-01-01 23:13:05 +0100

    2025 Day11

--- 2025/Day11/day11.py
+++ 2025/Day11/day11.py
@@ -37,21 +37,27 @@ print(f"Part One: {ans}")
 
 # =========== Part Two =========== #
 
-graph = load_data("./input.txt")
+from functools import cache
 
-ans = 0
-stack = deque([("svr", False, False)])
-while stack:
-  node, fft, dac = stack.popleft()
+
+@cache
+def steps_to(org: str, dst: str, avoid: tuple[str]) -> int:
   
-  if node == "out":
-    ans += (fft and dac)
-    continue
+  if org in avoid:
+    return 0
   
-  for nxt in graph[node]:
-    nx_fft = fft if fft else node == "fft"
-    nx_dac = dac if dac else node == "dac"
-    stack.append((nxt, nx_fft, nx_dac))
+  if org == dst:
+    return 1
+  
+  return sum(steps_to(nxt, dst, avoid) for nxt in graph[org])
 
 
-print(f"Part Two: {ans}")
+svr_fft = steps_to("svr", "fft", ("dac", "out"))
+svr_dac = steps_to("svr", "dac", ("fft", "out"))
+fft_dac = steps_to("fft", "dac", ("svr", "out"))
+dac_fft = steps_to("dac", "fft", ("svr", "out"))
+fft_out = steps_to("fft", "out", ("svr", "dac"))
+dac_out = steps_to("dac", "out", ("svr", "fft"))
+
+
+print(f"Part Two: {svr_fft * fft_dac * dac_out + svr_dac * dac_fft * fft_out}")
