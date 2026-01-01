6f7a87b13c90012218ff6acdbde047ba9990fe44
Victor Gonzalez Morales <victorgm2709@gmail.com>
2026-01-02 00:29:24 +0100

    2025 Day09

--- 2025/Day09/day09.py
+++ 2025/Day09/day09.py
@@ -19,6 +19,9 @@ print(f"Part One: {
 
 # =========== Part Two =========== #
 
+import shapely as sh
+
+
 def calc_area_points(x1, y1, x2, y2):
   top = min(y1, y2)
   left = min(x1, x2)
@@ -31,4 +34,14 @@ def calc_area_points(x1, y1, x2, y2):
   return area, tl, br
 
 
+max_area = 0
+figure = sh.Polygon(red_tiles)
+for i, p1 in enumerate(red_tiles):
+  for p2 in red_tiles[i+1:]:
+    area, tl, br = calc_area_points(*p1, *p2)
+    
+    if figure.contains(sh.box(*tl, *br)):
+      max_area = max(max_area, area)
 
+
+print(f"Part Two: {max_area}")
